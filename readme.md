841be3a15315984186c4d81992b4bb8649148599
Christoph Gockel <git@christophgockel.de>
2020-05-14 22:02:32 +0100

    Adds the main function that interacts with stdout

--- readme.md
+++ readme.md
@@ -30,6 +30,30 @@ cargo test
 `test` will automatically run all tests found in the source modules.
 
 
+### Testing Approach
+
+The test for function `example::message` is only verifying the return value of it.
+
+Testing `main::print_messaage` on the other hand is done via a test-double that gets injected.
+This allows us to _spy_ on the output it produces.
+We want to avoid printing anything to the screen while running the tests.
+Injecting a test double in this instance is a nice way to isolate our application from the command line.
+
+
+## Running the Application
+
+To run the application we can run it with cargo: `cargo run`.
+You should see the text &ldquo;Rust Example&rdquo; being printed.
+
+```
+$: cargo run
+   Compiling rust-command-line v0.1.0 (/home/vanilla/rust-command-line)
+    Finished dev [unoptimized + debuginfo] target(s) in 1.90s
+     Running `target/debug/rust-command-line`
+Rust Example
+```
+
+
 ## Built With
 
 - [Rust](https://www.rust-lang.org)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,50 @@
+use std::io::Error;
+use std::io::Write;
+
 mod example;
 
 fn main() {
-    println!("{}", example::message());
+    print_message(&mut std::io::stdout()).unwrap();
 }
 
+fn print_message(stdout: &mut dyn Write) -> Result<(), Error> {
+    stdout.write(format!("{}\n", example::message()).as_bytes())?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct StdoutDouble {
+        pub written_content: Option<String>,
+    }
+
+    impl StdoutDouble {
+        fn new() -> StdoutDouble {
+            StdoutDouble {
+                written_content: None,
+            }
+        }
+    }
+
+    impl Write for StdoutDouble {
+        fn write(&mut self, content: &[u8]) -> std::result::Result<usize, std::io::Error> {
+            self.written_content = Some(std::str::from_utf8(content).unwrap().to_string());
+
+            Ok(0)
+        }
+        fn flush(&mut self) -> std::result::Result<(), std::io::Error> {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn prints_message() {
+        let mut stdout = StdoutDouble::new();
+
+        print_message(&mut stdout).unwrap();
+
+        assert_eq!("Rust Example\n", stdout.written_content.unwrap())
+    }
+}
